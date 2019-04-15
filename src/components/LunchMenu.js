97818b4e0f4a77b04ff53d754db0a43deb9b3c5d
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-04-14 22:42:17 -0700

    Fixed navbar scrolling nullpointer bug

--- src/components/LunchMenu.js
+++ src/components/LunchMenu.js
@@ -7,6 +7,8 @@ class LunchMenu extends React.Component {
 
   
   componentDidMount() {
+    console.log("here");
+    console.log(window.location.href);
     window.onscroll = function() {myFunction()};
 
   }
@@ -14,6 +16,13 @@ class LunchMenu extends React.Component {
   render(){
     return (
       <div className="menuAndNavContainer">
+        <div id="lunchNav">
+            <h4> Menu Navigation </h4>
+            <a href="#lunchSpecials">Lunch Specials</a>
+            <a href="#lunchCombinations">Combinations</a>
+            <a href="#lunchSoup">Noodle Soup</a>
+            <a href="#lunchNoodles">Lunch Noodles</a>
+        </div>
         <div className="menuContainer">
           <img
             src="/menu/Lunch0Specials.png"
@@ -32,13 +41,6 @@ class LunchMenu extends React.Component {
             id="lunchNoodles"
           />
         </div>
-          <div id="lunchNav">
-              <h4> Menu Navigation </h4>
-              <Link to="#lunchSpecials">Lunch Specials</Link>
-              <a href="#lunchCombinations">Combinations</a>
-              <a href="#lunchSoup">Noodle Soup</a>
-              <a href="#lunchNoodles">Lunch Noodles</a>
-          </div>
       </div>
     )
     
@@ -47,19 +49,21 @@ class LunchMenu extends React.Component {
 }
 
 function myFunction() {
-    // Get the navbar
-    var navbar = document.getElementById("lunchNav");
+    if(window.location.href === "http://localhost:3000/LunchMenu") {
+      // Get the navbar
+      var navbar = document.getElementById("lunchNav");
 
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
+      // Get the offset position of the navbar
+      var sticky = navbar.offsetTop;
 
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }         
+      // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }         
+    }
 }
 
 export default LunchMenu
