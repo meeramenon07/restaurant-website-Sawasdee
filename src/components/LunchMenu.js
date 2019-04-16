2692779556f438a5b05fceedd4d8fc0a2f1a6699
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-04-16 16:30:31 -0700

    removed react-sticky module

--- src/components/LunchMenu.js
+++ src/components/LunchMenu.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { StickyContainer, Sticky } from 'react-sticky'
 //import '../css/NotHomepage.css';
 
 class LunchMenu extends React.Component {
